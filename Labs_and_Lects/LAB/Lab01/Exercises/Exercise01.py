cc50645af9826246b6744e6da4e145508cc132b5
NhutNguyen236 <nhutnguyenf330@gmail.com>
2021-09-15 09:40:43 +0700

    :zap: make read_file module

--- Labs_and_Lects/LAB/Lab01/Exercises/Exercise01.py
+++ Labs_and_Lects/LAB/Lab01/Exercises/Exercise01.py
@@ -1,14 +1,9 @@
 import cv2 as cv
 import numpy
 
-from pathlib import Path
+from read_file import read_image
 
-# get assets dir path
-ASSETS_DIR = (Path(__file__).resolve().parent.parent.parent).joinpath('assets')
-
-# get image exact path and convert to string for imread
-img_src = str(ASSETS_DIR.joinpath('lena.jpg'))
-image = cv.imread(img_src)
+image = read_image('lena.jpg')
 blue, green, red = cv.split(image)
 
 # create a zero matrix with the size of Blue
---
+++ Labs_and_Lects/LAB/Lab01/Exercises/Exercise02.py
@@ -0,0 +1,6 @@
+
+import cv2 as cv
+import numpy
+from read_file import read_image
+
+image = read_image('lena.jpg')
